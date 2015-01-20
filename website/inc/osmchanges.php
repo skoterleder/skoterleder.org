3511a456341961677711bc987535c2ed9e9c949a
Henrik Rosvall <henrik_rosvall@yahoo.se>
2015-01-20 21:44:23 +0100

    Save OSM changes in DB

---
+++ website/inc/osmchanges.php
@@ -0,0 +1,50 @@
+<?php
+include_once 'inc/database_config.php';
+
+$xml = file_get_contents("php://input") ;
+$row = explode('"', $xml);
+
+$user = "";
+$lat = "";
+
+$db->query("START TRANSACTION");
+
+for ($i = 0; $i < count($row); ++$i) {
+	
+	if ($row[$i] == " lat=") {
+		$lat = $row[$i+1];
+		$lon = "";
+		$changeset = "";
+		$user = "";
+		$uid = "";
+	}
+	if ($row[$i] == " lon=")       $lng  = $row[$i+1];
+	if ($row[$i] == " changeset=") $changeset = $row[$i+1];
+	if ($row[$i] == " uid=")       $uid  = $row[$i+1];
+	if ($row[$i] == " user=")      $user = $row[$i+1];
+	
+	if ($user && $lat) {
+		// Save data
+		echo "lat: $lat lng: $lng changeset: $changeset user: $user \n";
+		
+		/* Prepare statement */
+		$sql='INSERT INTO osmchange (lat, lng, changeset, user, uid,create_date) VALUES (?,?,?,?,?,CURDATE())';
+		$stmt = $db->prepare($sql);
+		if($stmt === false) {
+		  trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $db->error, E_USER_ERROR);
+		}
+		 
+		/* Bind parameters. TYpes: s = string, i = integer, d = double,  b = blob */
+		$stmt->bind_param('ddisi',$lat,$lng,$changeset,$user,$uid);
+		 
+		/* Execute statement */
+		$stmt->execute();
+		$stmt->close();
+		
+		$user = "";
+		$lat = "";
+	}
+}
+
+$db->query("COMMIT");
+?>
